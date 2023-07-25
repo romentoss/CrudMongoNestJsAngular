e9c2c43e4b9407d6129faab41368bb43a6b37010
Romentos <padilla.naranja@gmail.com>
2023-07-25 14:08:55 +0200

    backend in nest finish with create user and read, auth with JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,11 @@
 import { CreateUserDto } from './dto/create-user.dto';
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body,  UseGuards,Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UpdateAuthDto } from './dto/update-auth.dto';
-import { LoginDto } from './dto/login.dto';
+
+import { LoginDto ,  RegisterUserDto } from './dto/index';
+import { AuthGuard } from './guards/auth.guard';
+import { LoginResponse } from './interfaces/login-response';
+import { User } from './entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -17,28 +20,45 @@ export class AuthController {
 
   @Post('/login')
   login(@Body() loginDto: LoginDto) {
-
-    
     return this.authService.login(loginDto);
   }
 
+  @Post('/register')
+  register(@Body() registerDto: RegisterUserDto) {
+    return this.authService.register(registerDto);
+  }
+
+  @UseGuards(AuthGuard)
   @Get()
-  findAll() {
+  findAll( @Request() req:Request) {
+    // const user = req['user'];
+    // return user;
     return this.authService.findAll();
   }
+  @UseGuards(AuthGuard)
+  @Get('check-token')
+  checkToken(@Request() req:Request): LoginResponse{
+    const user = req['user'] as User;
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
+    return{
+      user,
+      token: this.authService.getJwtToken({id:user._id})
+    }
+
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.authService.findOne(+id);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
+  //   return this.authService.update(+id, updateAuthDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.authService.remove(+id);
+  // }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,13 +3,16 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 import { User } from './entities/user.entity';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+
 
 import * as bcryptjs  from 'bcryptjs'
-import { LoginDto } from './dto/login.dto';
+
+import {LoginDto,RegisterUserDto,CreateUserDto,UpdateAuthDto} from './dto/index';
+
 import { JwtService } from '@nestjs/jwt';
 import { JwtPayload } from './interfaces/jwt-payload';
+import { LoginResponse } from './interfaces/login-response';
+
 
 @Injectable()
 export class AuthService {
@@ -44,15 +47,19 @@ export class AuthService {
       if(error.code === 11000){
         throw new BadRequestException(`${createUserDto.email} already exists!`);
       }
-      throw new InternalServerErrorException('Something terrrible happend!!!')
-   
-      
+      throw new InternalServerErrorException('Something terrrible happend!!!')   
     }
-
-   
   }
 
-  async login(loginDto:LoginDto){
+
+  async register( registerDto:RegisterUserDto):Promise<LoginResponse>{
+    const user = await this.create(registerDto)
+    return {
+      user:user,
+      token: this.getJwtToken({id:user._id}),
+    };
+  }
+  async login(loginDto:LoginDto):Promise<LoginResponse>{
     const {email, password} = loginDto;
 
     const user = await this.userModel.findOne({email})
@@ -72,8 +79,14 @@ export class AuthService {
     }
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  findAll():Promise<User[]> {
+    return this.userModel.find()
+  }
+
+  async findUserById(id:string){
+    const user = await this.userModel.findById(id);
+    const {password, ...rest} = user.toJSON(); 
+    return rest;
   }
 
   findOne(id: number) {
