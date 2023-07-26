e463eab2cebb97fa7740c005dfbe88a02589c6f2
Romentos <padilla.naranja@gmail.com>
2023-07-26 17:52:29 +0200

    PORT log added

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,11 @@ async function bootstrap() {
       forbidNonWhitelisted:true
     })
   )
+
+  const PORT = process.env.PORT ?? 3000;
+
+  console.log(`App corriendo en puerto: ${PORT}`);
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
